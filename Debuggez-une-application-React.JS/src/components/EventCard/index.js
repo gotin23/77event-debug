b119710ca3f357955205714cacdbeb342925b37f
Hugo Gouzy <110604228+gotin23@users.noreply.github.com>
2023-05-02 22:22:01 +0200

    modif de helper et correction sur slider et eventcard

--- Debuggez-une-application-React.JS/src/components/EventCard/index.js
+++ Debuggez-une-application-React.JS/src/components/EventCard/index.js
@@ -3,43 +3,33 @@ import { getMonth } from "../../helpers/Date";
 
 import "./style.scss";
 
-const EventCard = ({
-  imageSrc,
-  imageAlt,
-  date = new Date(),
-  title,
-  label,
-  small = false,
-  ...props
-}) => (
-    <div
-      data-testid="card-testid"
-      className={`EventCard${small ? " EventCard--small" : ""}`}
-      {...props}
-    >
-      <div className="EventCard__imageContainer">
-        <img data-testid="card-image-testid" src={imageSrc} alt={imageAlt} />
-        <div className="EventCard__label">{label}</div>
-      </div>
-      <div className="EventCard__descriptionContainer">
-        <div className="EventCard__title">{title}</div>
-        <div className="EventCard__month">{getMonth(date)}</div>
-      </div>
+const EventCard = ({ imageSrc, imageAlt, date = new Date(), title, label, small = false, ...props }) => (
+  <div data-testid="card-testid" className={`EventCard${small ? " EventCard--small" : ""}`} {...props}>
+    <div className="EventCard__imageContainer">
+      <img data-testid="card-image-testid" src={imageSrc} alt={imageAlt} />
+      <div className="EventCard__label">{label}</div>
     </div>
-  );
-
+    <div className="EventCard__descriptionContainer">
+      <div className="EventCard__title">{title}</div>
+      <div className="EventCard__month">{getMonth(date)}</div>
+    </div>
+  </div>
+);
+// Je retire le require pour imagesrc,title mais j'ajoute une valeur par defaut(ici vide)
 EventCard.propTypes = {
-  imageSrc: PropTypes.string.isRequired,
+  imageSrc: PropTypes.string,
   imageAlt: PropTypes.string,
   date: PropTypes.instanceOf(Date).isRequired,
-  title: PropTypes.string.isRequired,
+  title: PropTypes.string,
   small: PropTypes.bool,
   label: PropTypes.string.isRequired,
 };
 
 EventCard.defaultProps = {
+  imageSrc: "",
+  title: "titre",
   imageAlt: "image",
   small: false,
-}
+};
 
 export default EventCard;
--- Debuggez-une-application-React.JS/src/containers/Slider/index.js
+++ Debuggez-une-application-React.JS/src/containers/Slider/index.js
@@ -8,38 +8,47 @@ const Slider = () => {
   const { data } = useData();
 
   const [index, setIndex] = useState(0);
-  // modification de la fonction de trie
-  const byDateDesc = data?.focus.sort((evtA, evtB) => (new Date(evtA.date) < new Date(evtB.date) ? 1 : -1));
+  // modification de la fonction de tri
+  const byDateDesc = data?.focus.sort((evtA, evtB) => (new Date(evtA.date) < new Date(evtB.date) ? -1 : 1));
   // Modification de la fonction next card avec ajout du -1 pour prendre en compte l index qui commence a 0
+  // Ajout d'une verification a la function nextcard
   const nextCard = () => {
-    setTimeout(() => setIndex(index < byDateDesc.length - 1 ? index + 1 : 0), 5000);
+    if (byDateDesc) {
+      setTimeout(() => setIndex(index < byDateDesc.length - 1 ? index + 1 : 0), 5000);
+    }
   };
-
+  // const changePicByDots = (idx) => {
+  //   console.log(idx);
+  //   setIndex(idx);
+  // };
   useEffect(() => {
     nextCard();
   });
   return (
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
-        <>
-          <div key={event.title} className={`SlideCard SlideCard--${index === idx ? "display" : "hide"}`}>
+        // ajout d'une div pour une key
+        <div key={event.title}>
+          <div className={`SlideCard SlideCard--${index === idx ? "display" : "hide"}`}>
             <img src={event.cover} alt="forum" />
             <div className="SlideCard__descriptionContainer">
               <div className="SlideCard__description">
                 <h3>{event.title}</h3>
                 <p>{event.description}</p>
+                {/* modification de la structure de de la variable month dans getMonth() */}
                 <div>{getMonth(new Date(event.date))}</div>
               </div>
             </div>
           </div>
           <div className="SlideCard__paginationContainer">
             <div className="SlideCard__pagination">
+              {/* correction du bug du bullet point avec la key et ajout de readonly pour eviter une erreur */}
               {byDateDesc.map((_, radioIdx) => (
-                <input key={`${event.id}`} type="radio" name="radio-button" checked={idx === radioIdx} />
+                <input key={`${radioIdx * 3}`} type="radio" name="radio-button" checked={radioIdx === index} readOnly />
               ))}
             </div>
           </div>
-        </>
+        </div>
       ))}
     </div>
   );
